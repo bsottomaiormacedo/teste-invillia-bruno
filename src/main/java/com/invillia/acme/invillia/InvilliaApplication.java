b41107190d3a2b77ab12d761bf527580556bb939
Bruno Macedo <bruno.sottomaior@castgroup.com.br>
2019-03-19 08:53:30 -0300

    Commit inicial

---
+++ src/main/java/com/invillia/acme/invillia/InvilliaApplication.java
@@ -0,0 +1,13 @@
+package com.invillia.acme.invillia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvilliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvilliaApplication.class, args);
+	}
+
+}
